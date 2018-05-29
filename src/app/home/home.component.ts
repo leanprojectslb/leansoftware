c96cdc53ada4969a6a61d1d8be7515c1f9f8da17
DIR\CKulkarni3 <ckulkarni3@slb.com>
2018-05-29 16:48:31 +0530

    add process functionality

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import {Component} from '@angular/core';
+import {Process} from '../model/Process';
+import {EntityQueue} from '../model/EntityQueue';
 
 @Component({
   selector: 'home',
@@ -6,4 +8,33 @@ import {Component} from '@angular/core';
   templateUrl: './home.component.html'
 })
 export class HomeComponent {
+
+  public backlog = new EntityQueue();
+  // set array for task
+
+   public finalQueue: EntityQueue;
+  // set array for task
+
+  public processCounter: number = 1;
+
+  public processList : Array<Process> = [];
+
+  addProcess() {
+  let process1 = new Process();
+  process1.capacity = 3;
+  process1.name = 'Process One'+ this.processCounter;
+  this.processCounter = this.processCounter + 1;
+  process1.outputQueue = new EntityQueue();
+  this.finalQueue = process1.outputQueue;
+  if(this.processCounter == 1){
+    process1.inputQueue = this.backlog;
+  } else {
+    process1.inputQueue = this.finalQueue;
+  }
+  this.processList.push(process1);
+  }
+
+  getProcesses () {
+    
+  }
 }
